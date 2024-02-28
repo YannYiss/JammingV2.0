2e63fbf1107ed85aac149d3042d8c1242962a614
Adrian Nieto <adrian_nc@outlook.com>
2024-02-28 14:41:43 -0600

    improved CSS responsive design

--- src/components/GoToButton/GoToButton.js
+++ src/components/GoToButton/GoToButton.js
@@ -4,7 +4,7 @@ import styles from './GoToButton.module.css';
 export default function GoToButton({hide}) {
     return (
         <div className={hide ? '' : styles.hideButton}>
-            <a href='#tracklist'><button className={styles.goToButton}>Go to Playlist</button></a>
+            <a href='#tracklist'><button className={styles.goToButton}>⬇</button></a>
         </div>
   )
 }
